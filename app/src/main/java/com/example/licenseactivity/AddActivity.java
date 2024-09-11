3a799f0be2ba937e04554f391d9f276e6884f39d
AlexUrsu28 <alexandru.ursulescu@student.upt.ro>
2024-09-11 02:20:30 -0700

    Avansare_Proiect

--- app/src/main/java/com/example/licenseactivity/AddActivity.java
+++ app/src/main/java/com/example/licenseactivity/AddActivity.java
@@ -1,6 +1,9 @@
 package com.example.licenseactivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,24 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class AddActivity extends AppCompatActivity {
 
+    EditText name_spital, name_departament, name_doctor;
+    Button add_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_add);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        name_spital = findViewById(R.id.name_spital);
+        name_departament = findViewById(R.id.name_department);
+        name_doctor = findViewById(R.id.name_doctors);
+        add_button = findViewById(R.id.add_button);
+        add_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MyDataBase mydb = new MyDataBase(AddActivity.this);
+                mydb.addMedical(name_spital.getText().toString().trim(),
+                        name_departament.getText().toString().trim(),
+                        name_doctor.getText().toString().trim());
+            }
         });
-    }
+        }
 }
--- app/src/main/java/com/example/licenseactivity/MyDataBase.java
+++ app/src/main/java/com/example/licenseactivity/MyDataBase.java
@@ -1,8 +1,10 @@
 package com.example.licenseactivity;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
@@ -11,7 +13,7 @@ public class MyDataBase extends SQLiteOpenHelper {
     private Context context;
     private static final String DATABASENAME = "HotSpitalDataBase.db";
     private static final int DATABASEVERSION = 1;
-    private static final String TABLENAME = "HotSpitalReseved";
+    private static final String TABLENAME = "HotSpitalReserved";
     private static final String COLUMNID = "ID";
     private static final String COLUMNNAME = "NameHotSpital";
     private static final String COLUMNDEPARTMENT = "Department";
@@ -23,7 +25,11 @@ public class MyDataBase extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String query = "CREATE_TABLE" + TABLENAME + "(" + COLUMNID + "INTEGER PRIMARY KEY AUTOINCREMENT, " +  COLUMNNAME + "TEXT," + COLUMNDEPARTMENT + "TEXT," + COLUMNDOCTORS + "TEXT)";
+        String query = "CREATE TABLE " + TABLENAME + " (" +
+                COLUMNID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMNNAME + " TEXT, " +
+                COLUMNDEPARTMENT + " TEXT, " +
+                COLUMNDOCTORS + " TEXT);";
         db.execSQL(query);
     }
 
@@ -31,5 +37,19 @@ public class MyDataBase extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLENAME);
         onCreate(db);
+    }
+
+    void addMedical(String spital, String departament, String doctor){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put(COLUMNNAME, spital);
+        cv.put(COLUMNDEPARTMENT, departament);
+        cv.put(COLUMNDOCTORS, doctor);
+        long result = db.insert(TABLENAME, null, cv);
+        if(result == -1)
+            Toast.makeText(context, "Failed", Toast.LENGTH_SHORT).show();
+        else{
+            Toast.makeText(context, "Added Succesfully", Toast.LENGTH_SHORT).show();
+        }
     }
 }
